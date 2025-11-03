01b18c6ea6f7530294a96843efb4bb748a85b797
Gabriel Djemal <gabrieljoseph.djemal@post.runi.ac.ilgit config --list>
2025-11-03 10:54:45 +0200

    Completed Assignment

--- NumWords.java
+++ NumWords.java
@@ -2,10 +2,9 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    int NumWords = Integer.parseInt(args[0]);
-		int thousands = NumWords / 1000;
 		int hundreds = (NumWords % 1000) / 100;
 		int tens = (NumWords % 100) / 10;
 		int ones = NumWords % 10;
-		System.out.println(thousands + " thousands, " + hundreds + " hundreds, " + tens + " tens, " + ones + " ones");
+		System.out.println(hundreds + " hundreds, " + tens + " tens, " + ones + " ones");
 	}
 }
